8da6e4d2d9a5e86d0530c962334292d0f9bf406b
Ian Andwati <andwatiian@gmail.com>
2024-12-19 18:35:47 +0300

    implemented the network client

--- internal/network/client.go
+++ internal/network/client.go
@@ -1,1 +1,36 @@
 package network
+
+import (
+	"net"
+	"time"
+)
+
+func SendDNSQuery(query []byte, server string) ([]byte, error) {
+	conn, err := net.Dial("udp", server)
+	if err != nil {
+		return nil, err
+	}
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+
+		}
+	}(conn)
+
+	err = conn.SetDeadline(time.Now().Add(DefaultTimeout))
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := conn.Write(query); err != nil {
+		return nil, err
+	}
+
+	response := make([]byte, 512)
+	n, err := conn.Read(response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response[:n], nil
+}
--- internal/network/config.go
+++ internal/network/config.go
@@ -1,1 +1,6 @@
 package network
+
+import "time"
+
+const DefaultServer = "8.8.8.8:53"
+const DefaultTimeout = 5 * time.Second
