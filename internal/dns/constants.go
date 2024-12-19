4ccc8bae999e88586c6603f09e2504be5749af39
Ian Andwati <andwatiian@gmail.com>
2024-12-19 18:27:11 +0300

    implemented query types constants

--- internal/dns/constants.go
+++ internal/dns/constants.go
@@ -1,1 +1,11 @@
 package dns
+
+const (
+	QTypeA     uint16 = 1
+	QTypeAAAA  uint16 = 28
+	QTypeCNAME uint16 = 30
+)
+
+const (
+	QClassIN uint16 = 1
+)
--- internal/dns/message.go
+++ internal/dns/message.go
@@ -1,1 +1,13 @@
 package dns
+
+type DNSMessage struct {
+	Header   DNSHeader
+	Question DNSQuestion
+}
+
+// convert dns message into a byte slice
+func (m *DNSMessage) Serialize() []byte {
+	header := m.Header.Serialize()
+	question := m.Question.Serialize()
+	return append(header, question...)
+}
