afcd7132f0b86d80581e1e527fdf1c3e98b75a52
Ian Andwati <andwatiian@gmail.com>
2024-12-19 18:45:34 +0300

    send the request to a name server and dump the response out.

--- main.go
+++ main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"fmt"
+	"github.com/andwati/dns-resolver/internal/dns"
+	"github.com/andwati/dns-resolver/internal/network"
+	"log"
+)
+
+func main() {
+	domain := "dns.google.com"
+
+	header := dns.DNSHeader{
+		ID:      dns.GenerateRandomID(),
+		Flags:   0x0100,
+		QDCount: 1,
+	}
+
+	question := dns.DNSQuestion{
+		QName:  domain,
+		QType:  dns.QTypeA,
+		QClass: dns.QClassIN,
+	}
+
+	message := dns.DNSMessage{
+		Header:   header,
+		Question: question,
+	}
+
+	serializedMessage := message.Serialize()
+
+	response, err := network.SendDNSQuery(serializedMessage, network.DefaultServer)
+
+	if err != nil {
+		log.Fatalf("Error sending DNS query: %s", err)
+	}
+
+	fmt.Printf("DNS Response: %x\n", response)
+}
