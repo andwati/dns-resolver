85da50dbc8c2631686827155bfbb85757478eaf7
Ian Andwati <andwatiian@gmail.com>
2024-12-19 17:28:43 +0300

    implemented utility functions

--- utilities.go
+++ utilities.go
@@ -1,1 +1,15 @@
 package main
+
+import "encoding/binary"
+
+func uint16ToBytes(val uint16) []byte {
+	buf := make([]byte, 2)
+	binary.BigEndian.PutUint16(buf, val)
+	return buf
+}
+
+func uint32ToBytes(val uint32) []byte {
+	buf := make([]byte, 4)
+	binary.BigEndian.PutUint32(buf, val)
+	return buf
+}
